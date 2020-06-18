e6c2a3cd66bed28548c9f486ed5acb6bef699410
almatroude <abdulaibawam@gmail.com>
2020-06-18 16:12:03 +0000

    update my github code

--- exercise1.cpp
+++ exercise1.cpp
@@ -1,33 +1,35 @@
-/*Write a program that calculates and prints the sum of all 
-the natural numbers divisible by either 3 or 5, up to a given 
-limit entered by the user.*/
+/*Write a program that calculates and prints 
+the sum of all the natural numbers divisible by 
+either 3 or 5, up to a given limit entered by the user. */
 
+/* ANALYSIS */
 
-// C++ program to print all the natural numbers
-// divisible by 3 and 5 for a given number
+// formula to calculate the sum of numbers divisible by 3 or 4 from 1 to the limit the user want
+// sum Sn= n/2 [2a + (n - 1)d].
+// where n is the total number of terms in the series
+// ..... a is the first term
+// ..... d is the common difference
 #include <iostream>
 using namespace std;
 
-
-void result(int n)
+int sum(int N)
 {
+    int S1, S2;
 
-    for (int num = 0; num < n; num++)
-    {
-        
-        if (num % 3 == 0 && num % 5 == 0)
-            cout << num << " ";
-    }
+    S1 = ((N / 3)) * (2 * 3 + (N / 3 - 1) * 3) / 2;
+    S2 = ((N / 4)) * (2 * 4 + (N / 4 - 1) * 4) / 2;
+
+    return S1 + S2;
 }
 
-
+// main function;
 int main()
 {
-    
-    int n;
-    cout << "please enter the limit of the numbers divisible both 3 and 5 \n";
-    cin >> n;
-    
-    result(n);
+    int N;
+    cout << "please enter the number that you want to find the sum of all natural numbers  divisible by 3 or 4, from 1 to the number you want " << endl;
+    cin >> N;
+
+    cout << "the sum of the numbers divisible by 3 or 4  from 1 up " << N << " is  " << sum(N) << endl;
+
     return 0;
 }
